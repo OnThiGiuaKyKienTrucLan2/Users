e515da4109aff7f427f9a8c8bb244fa55baa6c75
ThanhVinh2712 <you@example.com>
2021-10-31 00:00:14 +0700

    Cache

--- src/main/java/com/example/users/Controller/UserController.java
+++ src/main/java/com/example/users/Controller/UserController.java
@@ -3,7 +3,9 @@ import com.example.users.Entity.User;
 import com.example.users.Service.UserService;
 import com.example.users.VO.ResponseTemplateVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/users")
@@ -13,6 +15,7 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/{id}")
+    @Cacheable("Vinh")
     public ResponseTemplateVO getUserWithDepartment(@PathVariable("id")
                                                             Long userId){
         return userService.getUserWithDepartment(userId);
